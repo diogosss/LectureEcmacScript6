cee13648300cc6de4bfc952c27e04244e39cd180
Diego Salazar <diogol11ldu20@hotmail.com>
2022-05-10 17:43:16 -0500

    module.js Import

--- src/es6/index.js
+++ src/es6/index.js
@@ -97,5 +97,154 @@ const a = "a";
 
   
 
-//********** a  ************/
+//********** Objeots Mejorada  ************/
 
+let name_ = "diogo";
+let age_ = 32;
+
+obj_ = {name: name_, age: age_}; //forma antigua
+
+obj_S6 = {name_, age_}; //forma nueva S6
+
+console.log(obj_);
+
+console.log(obj_S6);
+
+
+//********** ARROWS Functions  ************/
+
+const names = [
+    {name: "diogo", age: "32"},
+    {name: "jessica", age: "27"}
+]
+
+//iterar antiguo
+let listOfnames = names.map(
+    function (item){
+        console.log(item.name);
+    }
+);
+
+
+//forma 1 S6 
+let listOfnames1 = names.map(item => console.log(item.name));
+
+//forma 2 S6 
+let listOfnames2 = (name, age, country ) => {
+    //code function
+}
+
+//forma 3 S6 
+let listOfnames3 = name => {
+    //code function
+}
+
+//forma 4 S6 
+const square = num => num * num;
+
+console.log(square(2));
+
+
+//********** PROMESAS para Asynchronismo  ************/
+
+const helloPromise = () => {
+    return new Promise((resolve, reject) => {
+        if(true){
+            resolve("Hey! OK!!");
+        }else{
+            reject("Ups!! ERROR!!");
+        }
+    });
+}
+
+helloPromise()
+    .then(response => console.log(response))
+    .then(() => console.log("hola"))
+    .catch(error => console.log(error));
+
+
+
+//--------------------------------------------------------------------------------------------
+
+
+//********** Clases, Módulos y Generadores  ************/
+
+class calculator {
+    constructor(){
+        this.valorA = 0;
+        this.valorB = 0;
+    }
+
+    sum(valorA, valorB){
+        this.valorA = valorA;
+        this.valorB = valorB;
+        return this.valorA + this.valorB;
+    }
+}
+
+const calc = new calculator();
+console.log(calc.sum(2,2));
+
+
+//********** Import Export modules ************/
+
+//import { hello } from './module.js'; //forma nueva NO FUNCA AQUI
+
+const hello = require('./module.js')
+
+hello(); //forma nueva
+
+console.log(hello()); 
+
+
+//********** GENERATORS ************/
+
+function* helloWorld() {
+    if(true){
+        yield 'Hello, ';
+    }
+    if (true){
+        yield 'World!!'
+    }
+};
+
+const generatorHello = helloWorld();
+
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+
+//********** FIBONACCI CON GENERATORS ************/
+
+function* fibonacci(){
+    var fn1 = 1;
+    var fn2 = 1;
+    while (true){  
+      var actual = fn2;
+      fn2 = fn1;
+      fn1 = fn1 + actual;
+      var reset = yield actual;
+      if (reset){
+          fn1 = 1;
+          fn2 = 1;
+      }
+    }
+  }
+  
+  var secuencia = fibonacci();
+  console.log(secuencia.next().value);     // 1
+  console.log(secuencia.next().value);     // 1
+  console.log(secuencia.next().value);     // 2
+  console.log(secuencia.next().value);     // 3
+  console.log(secuencia.next().value);     // 5
+  console.log(secuencia.next().value);     // 8
+  console.log(secuencia.next().value);     // 13
+  console.log(secuencia.next(true).value); // 1 //reset
+  console.log(secuencia.next().value);     // 1
+  console.log(secuencia.next().value);     // 2
+  console.log(secuencia.next().value);     // 3
+  console.log(secuencia.next().value); 
+  console.log(secuencia.next().value); 
+  console.log(secuencia.next().value); 
+  console.log(secuencia.next().value); 
+  console.log(secuencia.next().value); 
---
+++ src/es6/module.js
@@ -0,0 +1,8 @@
+
+const hello = () => {
+    return 'hello!!';
+}
+
+//export default hello; //forma nueva
+
+module.exports = hello; //forma antigua
